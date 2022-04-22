5e701da93530dad34b2181f7cfb2c0ff942f8971
Rabindra <drarabin@gmail.com>
2022-04-22 21:59:51 +0545

    arrow functions and this keyword

--- arrow_func_and_this.js
+++ arrow_func_and_this.js
@@ -1,9 +1,12 @@
 const person = {
     talk() {
-        var self = this;
-        setTimeout(function () {
-            console.log("this", this);  // returns reference to window object
-            console.log("this", self);  // returns person object
+        // var self = this;
+        setTimeout(() => {
+            // returns reference to person object because of 
+            // usage of arrow function
+            // arrow functions donot rebind the this keyword
+            console.log("this", this);
+            // console.log("this", self);  // returns person object
         }, 1000);
         console.log("this", this);  // returns person object
     }
